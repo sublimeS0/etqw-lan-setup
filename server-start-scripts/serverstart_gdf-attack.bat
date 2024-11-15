8e141ae4e3b9186f8b9d0fe54e5a3eea681ad70b
sublimeS0 <sublimee.S0@gmail.com>
2024-11-15 09:44:34 -0500

    Removed deprecated server start process files

--- server-start-scripts/serverstart_gdf-attack.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\id Software\Enemy Territory - QUAKE Wars\etqwded.exe" +set net_serverDedicated 1 +set net_port 27733 +exec "C:\Program Files (x86)\id Software\Enemy Territory - QUAKE Wars\server-configs\gdf_attack.cfg" +set fs_savepath "C:\Program Files (x86)\id Software\Enemy Territory - QUAKE Wars"
